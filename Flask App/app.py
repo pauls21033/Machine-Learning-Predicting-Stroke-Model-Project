5fc14b3aeae59530b01432baa098bbd3ca9f09a8
unknown <slquevedo00@gmail.com>
2022-12-01 20:08:50 -0600

    updated flask app to read sqlite

---
+++ Flask App/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+#from flask_marshmallow import Marshmallow
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Database
+#app.config['SQLALCHEMY_DATABASE_URI'] = ('sqlite:///' + 
+ #   os.path.join(basedir, '..Resources/stroke_analysis.sqlite'))
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Init db
+engine = create_engine("sqlite:///Resources/stroke_df.sqlite")
+Base = automap_base()
+
+#reflect the tables
+Base.prepare(engine, reflect=True)
+
+stroke_db = Base.classes.stroke_df
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- app.py
+++ app.py
@@ -1,4 +1,23 @@
-from flask import Flask
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import datetime as dt
+
+from flask import Flask, jsonify
+
+
+# Database Setup
+
+engine = create_engine("sqlite:///stroke_analysis.sqlite")
+
+#reflect an existing database into a new mocel
+Base = automap_base()
+
+#reflect the tables
+Base.prepare(engine, reflect=True)
 
 app = Flask(__name__)
 
