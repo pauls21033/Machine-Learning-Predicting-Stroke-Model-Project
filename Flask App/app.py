7a8f645bd8c7c4280ea056d00fd7a1032d4f7adb
Austin Calvo <austindcalvo@Tilleys-Air.lan>
2022-12-01 21:38:55 -0600

    updated app.py

--- Flask App/app.py
+++ Flask App/app.py
@@ -12,13 +12,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
  #   os.path.join(basedir, '..Resources/stroke_analysis.sqlite'))
 #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Init db
-engine = create_engine("sqlite:///Resources/stroke_df.sqlite")
+engine = create_engine("sqlite:///stroke_analysis.sqlite", echo=True)
 Base = automap_base()
 
 #reflect the tables
 Base.prepare(engine, reflect=True)
 
-stroke_db = Base.classes.stroke_df
+stroke_db = Base.classes.stroke_analysis
 
 app = Flask(__name__)
 model = pickle.load(open('ML_Test.pkl', 'rb')) 
