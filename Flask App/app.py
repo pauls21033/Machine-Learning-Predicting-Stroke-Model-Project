9d03e918c728ca5e3778188d14dbc87c02c35270
unknown <slquevedo00@gmail.com>
2022-12-05 15:57:23 -0600

    removed unnecessary lines

--- Flask App/app.py
+++ Flask App/app.py
@@ -1,18 +1,11 @@
 from flask import Flask, request, render_template
-import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-#from flask_marshmallow import Marshmallow
 import os
 import pickle
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-# Database
-#app.config['SQLALCHEMY_DATABASE_URI'] = ('sqlite:///' + 
- #   os.path.join(basedir, '..Resources/stroke_analysis.sqlite'))
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# Init db
+
 engine = create_engine("sqlite:///stroke_analysis.sqlite3", echo=True)
 Base = automap_base()
 
@@ -46,7 +39,7 @@ def predict():
     output = round(prediction[0], 2) 
 
     return render_template('prediction.html', prediction_text=f'{age}, {gender}, {blood_pressure}, {glucose} , {smoking}, {bmi}, {cholesterol} : {output}')    
-    #return render_template('predict.html')
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
