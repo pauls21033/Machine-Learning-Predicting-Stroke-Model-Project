763ff0c209de19c6f265d74f713ffc8c64becd6e
unknown <slquevedo00@gmail.com>
2022-12-06 19:45:41 -0600

    changes to results in app

--- Flask App/app.py
+++ Flask App/app.py
@@ -64,9 +64,9 @@ def result():
     bmi = data.get('bmi')
     cholesterol = data.get("cholesterol")
     survey = {"age": age, "gender": gender, "s_blood_pressure": s_blood_pressure, "d_blood_pressure": d_blood_pressure, "glucose": glucose, "smoking": smoking, "bmi": bmi, "cholesterol": cholesterol}
-    #prediction = model.predict([[age, gender, s_blood_pressure,d_blood_pressure, glucose, smoking, bmi, cholesterol]]) 
-    #output = round(prediction[0], 2) 
-    return jsonify(survey)
+    prediction = model.predict([[age, gender, s_blood_pressure,d_blood_pressure, glucose, smoking, bmi, cholesterol]]) 
+    output = round(prediction[0], 2) 
+    return jsonify(output)
 
 @app.route('/api/prediction',methods=['POST'])
 def prediction():
