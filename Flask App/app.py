898c3d9de8fa00a5de1a26cc9932c32d989ef8ef
unknown <slquevedo00@gmail.com>
2022-12-06 21:55:32 -0600

    got things working

--- Flask App/app.py
+++ Flask App/app.py
@@ -3,33 +3,26 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy import create_engine
 import os
 import pickle
-
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 engine = create_engine("sqlite:///stroke_analysis.sqlite3", echo=True)
 Base = automap_base()
-
 #reflect the tables
 Base.prepare(engine, reflect=True)
-
 stroke_db = Base.classes.stroke_analysis
 
 app = Flask(__name__)
+model = pickle.load(open('ML_Test.pkl', 'rb')) 
 model = pickle.load(open('Model.pkl', 'rb')) 
 
 @app.route("/")
 def home():
     return render_template('index.html')
-
 @app.route("/data")
 def data():
     return render_template('data.html')
-
 @app.route("/visualizations")
 def vis():
     return render_template('Images.html')
-
-
 #,methods=['POST']
 @app.route('/prediction')
 def predict():
@@ -44,9 +37,7 @@ def predict():
         
     #prediction = model.predict([[age, gender, s_blood_pressure, d_blood_pressure, glucose, smoking, bmi, cholesterol]]) 
     #output = round(prediction[0], 2) 
-
     return render_template('prediction.html')  
-
 @app.route("/api/predict", methods=['GET','POST'])  
 def result():
     survey = {}
@@ -64,26 +55,10 @@ def result():
     bmi = data.get('bmi')
     cholesterol = data.get("cholesterol")
     model = pickle.load(open('Model.pkl', 'rb')) 
-    #survey = {"age": age, "gender": gender, "s_blood_pressure": s_blood_pressure, "d_blood_pressure": d_blood_pressure, "glucose": glucose, "smoking": smoking, "bmi": bmi, "cholesterol": cholesterol}
+    survey = {"age": age, "gender": gender, "s_blood_pressure": s_blood_pressure, "d_blood_pressure": d_blood_pressure, "glucose": glucose, "smoking": smoking, "bmi": bmi, "cholesterol": cholesterol}
     prediction = model.predict([[age, gender, s_blood_pressure,d_blood_pressure, glucose, smoking, bmi, cholesterol]]) 
-    
-    return render_template('result.html', prediction_text=f'you are at a risk for a level {prediction[0]} stroke')
-
-@app.route('/api/prediction',methods=['POST'])
-def prediction():
-    age = request.form('age')
-    gender = request.form('Gender')
-    s_blood_pressure = request.form('Sys blood pressure')
-    d_blood_pressure = request.form('Dia blood pressure')
-    glucose = request.form('glucose')
-    smoking = request.form('Smoking')
-    bmi = request.form('BMI')
-    cholesterol = request.form("Cholesterol")
-    prediction = model.predict([[age, gender, s_blood_pressure, d_blood_pressure, glucose, smoking, bmi, cholesterol]])
-    output = prediction[0]
-    return jsonify(output)
-
-    return render_template('result.html', prediction_text=f'You are at risk for a level {output} stroke')
+    return render_template('result.html', prediction_text = f'you are at risk for a level {prediction} stroke')
+    #return render_template('result.html', prediction_text=f'you are at a risk for a {output} stroke')
 
 if __name__ == "__main__":
     app.run(debug=True)
